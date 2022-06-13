be7e4c1b99f8d9d83777e25eb2ec611b39769506
Juan Reynoso <reynoso.juan904@gmail.com>
2022-06-13 12:11:05 -0300

    Primer Commit

---
+++ ejercicio_VIII/tp_VIII_prueba/formverde.js
@@ -0,0 +1,64 @@
+const addButton = document.querySelector(".addButton");
+const input = document.querySelector(".input");
+const container = document.querySelector("container");
+
+class item {
+    constructor(itemName){
+        // Create the item div
+        this.createDiv(itemName);
+    }
+
+    createDiv(itemName) {
+        let input = document.createElement("input");
+        input.value = itemName;
+        input.disabled = true;
+        input.classList.add("item_input");
+        input.type = "text";
+
+        let itemBox = document.createElement("div");
+        itemBox.classList.add("item");
+
+        let editButton = document.createElement("button");
+        editButton.innerHTML = "EDIT";
+        editButton.classListadd("editButton");
+
+        let removeButton = document.createElement("button");
+        removeButton.innerHTML = "REMOVE";
+        editButton.classListadd("removeButton");
+
+        container.appendChild(input);
+
+        itemBox.appendChild(input);
+        itemBox.appendChild(editButton);
+        itemBox.appendChild(removeButton);
+
+        editButton.addEventListener( "click", () => this.edit(input));
+
+        removeButton.addEventListener( "click", () => this.edit(itemBox));
+    }
+
+    edit (input) {
+
+        input.disabled = !input.disabled;
+    }
+
+    remove (item) {
+        container.removeChild(item);
+    }
+}
+
+function check () {
+    if(input.value != "") {
+
+        new item(imput.value);
+        input.value = "";
+    }
+}
+
+addButton.addEventListener("click", check);
+window.addEventListener("keydown", (e) => {
+    if(e.winch == 13) {
+        check();
+    }
+})
+
