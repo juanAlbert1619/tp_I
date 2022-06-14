d3c4cf83f4a03eda5c8e81bd1b174e6b687574ad
Juan Reynoso <reynoso.juan904@gmail.com>
2022-06-13 21:23:57 -0300

    nuevo

--- ejercicio_VI/listTareasPendientes.js
+++ ejercicio_VI/listTareasPendientes.js
@@ -1,44 +1,101 @@
-const clave = "lista_tareas";
-
-
+const CLAVE_LOCALSTORAGE = "lista_tareas";
 document.addEventListener("DOMContentLoaded", () => {
-
     let tareas = [];
+
     const $contenedorTareas = document.querySelector("#contenedorTareas"),
-          $btnGuardarTare = document.querySelector("#btnAgregarTarea"),
-          $inputNuevaTarea = document.querySelector("#inputNuevoTarea");
+        $btnGuardarTarea = document.querySelector("#btnAgregarTarea"),
+        $inputNuevaTarea = document.querySelector("inputNuevaTarea");
 
-    //Escuchar el click del Button
+        // Escuchar Clic del Button para agregar tarea
 
-    buttonIngreso.onclick = () => {
+        $btnGuardarTarea.onclick = () => {
 
-        const tarea = inputNuevoIngreso.ariaValueMax;
-        if (!tarea) {
-            return;
-        }
+            const tarea = $inputNuevaTarea.value;
+            if(!tarea) {
+                return;
 
-        tareas.push({
-            tarea: tarea,
-            terminada: false,
-        });
+            }
 
-        inputNuevoIngreso.value = "";
-        buttonIngreso();
-        actualizarListTarea();
+            tareas.push({
+                tarea: tarea,
+                terminada: false,
+            });
+
+            $inputNuevaTarea.value = "";
+            guardarTareasEnAlmacenamiento();
+            refrescarListaDeTareas();
+        };
+        
+
+        
+        
+
+        const obtenerTareasDeAlmacenamiento = () => {
+            const posibleLista = JSON.parse(localStorage.getItem(CLAVE_LOCALSTORAGE));
+            if (posibleLista) {
+                return posibleLista;
+            } else {
+                return [];
+            }
+        };
+
+        const guardaTareaAlmacenamiento = () => {
+            localStorage.setItem(clave, JSON.stringify(tareas));
+        };
+
+        const refrescarListaDeTareas = () => {
+            $contenedorTareas.innerHTML = "";
+            for (const[indice, tarea] of tareas.entries()) {
+
+                const $enlaceParaEliminar = document.createElement("a");
+                $enlaceParaEliminar.classList.add("enlace-eliminar");
+                $enlaceParaEliminar.innerHTML = "&times;";
+                $enlaceParaEliminar.href = "";
+                $enlaceParaEliminar.onclick = (evento) => {
+                    evento.presentDefaul();
+                    if (!confirm("¿Quieres ELIMINAR la Tarea?")) {
+                        return;
+                    }
+                    tareas.splice(indice, 1);
+
+                    guardarTareasEnAlmacenamiento(tareas);
+                    refrescarListaDeTareas();
+                };
+
+                const $checkbox = document.createElement("input");
+                $checkbox.type = "checkbox";
+                $checkbox.onchange = function (){
+                    if(this.$checkd) {
+                        tareas[indice].terminada = true;
+
+                    }else {
+                        tareas[indice].terminada = false;
+                    }
+
+                    guardaTareaAlmacenamiento(tareas);
+                    refrescarListaDeTareas();
+                }
+
+                const $span = document.createElement("span");
+                $span.textContent = tarea.tarea;
+
+                const $li = document.createElement("li");
+
+                if(tarea.terminada) {
+                    $checkbox.checked = true;
+                    $span.classList.add("tachado");
+                }
+
+                $li.appendChild($checkbox);
+                $li.appendChild($span);
+                $li.appendChild($enlaceParaEliminar);
+                $contenedorTareas.appendChild($li);
+            }
+    
     };
 
-    const obtenerTareaAlmacenamiento = () => {
-        const posibleLista = JSON.parse(localStorage.getItem(clave))
-        if (posibleLista) {
-            return posibleLista;
-        } else {
-            return [];
-        }
-    }
-
-    const guardaTareaAlmacenamiento = () => {
-        localStorage.setItem(clave, JSON.stringify(tareas))
-    };
+    tareas = obtenerTareasDeAlmacenamiento();
+    refrescarListaDeTareas();
 
 
 })
